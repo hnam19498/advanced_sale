86c23187ee7c07447b374d1cb4b5250dfc1f90c6
HoangNam <hnam.19498@outlook.com>
2022-11-28 13:52:28 +0700

    fix

--- models/partner_vip.py
+++ models/partner_vip.py
@@ -7,20 +7,20 @@ class PartnerVip(models.Model):
     customer_discount_code = fields.Char(string='Discount code', default='')
 
     def open_vip_view(self):
-        selected_res_ids = []
+        partner_vip_ids = []
         res_ids = self.env['res.partner'].sudo().search([])
         for rec in res_ids:
             if rec.customer_discount_code:
                 if len(rec.customer_discount_code) == 6:
                     if rec.customer_discount_code[:3] == 'vip':
                         if int(rec.customer_discount_code[-2:]):
-                            selected_res_ids.append(rec.id)
+                            partner_vip_ids.append(rec.id)
 
         return {
             'name': 'Vip Customer',
             'type': 'ir.actions.act_window',
             'view_mode': 'tree,form',
             'target': 'current',
-            'domain': [('id', 'in', selected_res_ids)],
+            'domain': [('id', 'in', partner_vip_ids)],
             'res_model': 'res.partner',
         }
