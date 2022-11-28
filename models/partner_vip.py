4f976f05988ee2e66e4f24bbf4a9f334f85554d3
HoangNam <hnam.19498@outlook.com>
2022-11-28 10:16:00 +0700

    done

--- models/partner_vip.py
+++ models/partner_vip.py
@@ -5,3 +5,22 @@ class PartnerVip(models.Model):
     _inherit = 'res.partner'
 
     customer_discount_code = fields.Char(string='Discount code', default='')
+
+    def open_vip_view(self):
+        selected_res_ids = []
+        res_ids = self.env['res.partner'].sudo().search([])
+        for rec in res_ids:
+            if rec.customer_discount_code:
+                if len(rec.customer_discount_code) == 6:
+                    if rec.customer_discount_code[:3] == 'vip':
+                        if int(rec.customer_discount_code[-2:]):
+                            selected_res_ids.append(rec.id)
+
+        return {
+            'name': 'Vip Customer',
+            'type': 'ir.actions.act_window',
+            'view_mode': 'tree,form',
+            'target': 'current',
+            'domain': [('id', 'in', selected_res_ids)],
+            'res_model': 'res.partner',
+        }
--- models/sale_line.py
+++ models/sale_line.py
@@ -21,9 +21,6 @@ class SaleLine(models.Model):
 
     @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id')
     def _compute_amount(self):
-        """
-        Compute the amounts of the SO line.
-        """
         for line in self:
             price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
             taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty,
